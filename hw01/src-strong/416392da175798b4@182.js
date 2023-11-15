3a70337fbc761a4c72db86dcb0309ab48ae69a4e
smilesnailll <yuchen.yang.704@gmail.com>
2023-11-15 17:52:40 +0800

    test

--- hw01/src-strong/416392da175798b4@182.js
+++ hw01/src-strong/416392da175798b4@182.js
@@ -1,195 +1,98 @@
 function _1(md){return(
-  md`# HW1 strong baseline`
-  )}
+md`<div style="color: grey; font: 13px/25.5px var(--sans-serif); text-transform: uppercase;"><h1 style="display: none;">Zoom to bounding box</h1><a href="https://d3js.org/">D3</a> › <a href="/@d3/gallery">Gallery</a></div>
+
+# Zoom to bounding box
+
+Pan and zoom, or click to zoom into a particular state using [*zoom*.transform](https://d3js.org/d3-zoom#zoom_transform) transitions. The bounding box is computed using [*path*.bounds](https://d3js.org/d3-geo/path#path_bounds).`
+)}
+
+function _chart(d3,topojson,us)
+{
+  const width = 975;
+  const height = 610;
+
+  const zoom = d3.zoom()
+      .scaleExtent([1, 8])
+      .on("zoom", zoomed);
+
+  const svg = d3.create("svg")
+      .attr("viewBox", [0, 0, width, height])
+       .attr("width", width)
+      .attr("height", height)
+      .attr("style", "max-width: 100%; height: auto;")
+      .on("click", reset);
+
+  const path = d3.geoPath();
+
+  const g = svg.append("g");
+
+  const states = g.append("g")
+      .attr("fill", "#444")
+      .attr("cursor", "pointer")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .join("path")
+      .on("click", clicked)
+      .attr("d", path);
   
-  function _data1(__query,FileAttachment,invalidation){return(
-  __query(FileAttachment("data@1.csv"),{from:{table:"data"},sort:[],slice:{to:null,from:null},filter:[],select:{columns:null}},invalidation)
-  )}
-  
-  function _plot2(Inputs){return(
-  Inputs.form({
-    mt:  Inputs.range([0, 100], {label: "marginTop", step: 1}),
-    mr:  Inputs.range([0, 100], {label: "marginRight", step: 1}),
-    mb:  Inputs.range([0, 100], {label: "marginBottom", step: 1}),
-    ml:  Inputs.range([0, 100], {label: "marginLeft", step: 1}),
-  })
-  )}
-  
-  async function _4(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業一",
-        fill: "#164863",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _5(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業二",
-        fill: "#427D9D",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _6(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業三",
-        fill: "#9BBEC8",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _7(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業三",
-        fill: "#DDF2FD",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _8(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業四",
-        fill: "#86A789",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _9(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業五",
-        fill: "#B2C8BA",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _10(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業六",
-        fill: "#D2E3C8",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _11(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業七",
-        fill: "#EBF3E8",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _12(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業八",
-        fill: "#04364A",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _13(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業九",
-        fill: "#176B87",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  async function _14(Plot,FileAttachment){return(
-  Plot.plot({
-    marks: [
-      Plot.barY(await FileAttachment("data@1.csv").csv({ typed: "auto" }), {
-        x: "學號",
-        y: "作業十",
-        fill: "#64CCC5",
-        tip: true
-      }),
-      Plot.ruleY([0])
-    ]
-  })
-  )}
-  
-  export default function define(runtime, observer) {
-    const main = runtime.module();
-    function toString() { return this.url; }
-    const fileAttachments = new Map([
-      ["data@1.csv", {url: new URL("./data.csv", import.meta.url), mimeType: "text/csv", toString}]
-    ]);
-    main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
-    main.variable(observer()).define(["md"], _1);
-    main.variable(observer("data1")).define("data1", ["__query","FileAttachment","invalidation"], _data1);
-    main.variable(observer("viewof plot2")).define("viewof plot2", ["Inputs"], _plot2);
-    main.variable(observer("plot2")).define("plot2", ["Generators", "viewof plot2"], (G, _) => G.input(_));
-    main.variable(observer()).define(["Plot","FileAttachment"], _4);
-    main.variable(observer()).define(["Plot","FileAttachment"], _5);
-    main.variable(observer()).define(["Plot","FileAttachment"], _6);
-    main.variable(observer()).define(["Plot","FileAttachment"], _7);
-    main.variable(observer()).define(["Plot","FileAttachment"], _8);
-    main.variable(observer()).define(["Plot","FileAttachment"], _9);
-    main.variable(observer()).define(["Plot","FileAttachment"], _10);
-    main.variable(observer()).define(["Plot","FileAttachment"], _11);
-    main.variable(observer()).define(["Plot","FileAttachment"], _12);
-    main.variable(observer()).define(["Plot","FileAttachment"], _13);
-    main.variable(observer()).define(["Plot","FileAttachment"], _14);
-    return main;
+  states.append("title")
+      .text(d => d.properties.name);
+
+  g.append("path")
+      .attr("fill", "none")
+      .attr("stroke", "white")
+      .attr("stroke-linejoin", "round")
+      .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+
+  svg.call(zoom);
+
+  function reset() {
+    states.transition().style("fill", null);
+    svg.transition().duration(750).call(
+      zoom.transform,
+      d3.zoomIdentity,
+      d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
+    );
   }
+
+  function clicked(event, d) {
+    const [[x0, y0], [x1, y1]] = path.bounds(d);
+    event.stopPropagation();
+    states.transition().style("fill", null);
+    d3.select(this).transition().style("fill", "red");
+    svg.transition().duration(750).call(
+      zoom.transform,
+      d3.zoomIdentity
+        .translate(width / 2, height / 2)
+        .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+        .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+      d3.pointer(event, svg.node())
+    );
+  }
+
+  function zoomed(event) {
+    const {transform} = event;
+    g.attr("transform", transform);
+    g.attr("stroke-width", 1 / transform.k);
+  }
+
+  return svg.node();
+}
+
+
+function _us(FileAttachment){return(
+FileAttachment("states-albers-10m.json").json()
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  function toString() { return this.url; }
+  const fileAttachments = new Map([
+    ["states-albers-10m.json", {url: new URL("./files/75faaaca1f1a4f415145b9db520349a3a0b93a53c1071346a30e6824586a7c251f45367d9262ed148b7a2b5c2694aa7703f3ac88051abc65066fd0074fdf9c9e.json", import.meta.url), mimeType: "application/json", toString}]
+  ]);
+  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer("chart")).define("chart", ["d3","topojson","us"], _chart);
+  main.variable(observer("us")).define("us", ["FileAttachment"], _us);
+  return main;
+}
